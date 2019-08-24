108a6fe873cd9d0e1a9739755854000064c8effb
2gury <54157924+2gury@users.noreply.github.com>
2019-08-24 13:57:18 +0300

    delete Xcode description

--- Builder/Builder.cpp
+++ Builder/Builder.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Builder
-//
-//  Created by Egor Timonin on 04/08/2019.
-//  Copyright © 2019 Egor Timonin. All rights reserved.
-//
-
 #include <iostream>
 #include <vector>
 
