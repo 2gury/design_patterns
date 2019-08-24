effdcfda56a3a416fe2e0896ebf081e7aa720949
2gury <54157924+2gury@users.noreply.github.com>
2019-08-24 14:00:33 +0300

    delete Xcode description

--- Decorator/Decorator.cpp
+++ Decorator/Decorator.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Decorator
-//
-//  Created by Egor Timonin on 16/08/2019.
-//  Copyright © 2019 Egor Timonin. All rights reserved.
-//
-
 #include <iostream>
 
 class Component {
