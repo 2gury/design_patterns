93d24acd8ad05028feb2cd1b16f74409e612a3c4
2gury <54157924+2gury@users.noreply.github.com>
2019-08-24 12:56:33 +0300

    add AbstractFactory pattern

---
+++ AbstractFactory/main.cpp
@@ -0,0 +1,88 @@
+//
+//  main.cpp
+//  AbstractFactory
+//
+//  Created by Egor Timonin on 03/08/2019.
+//  Copyright © 2019 Egor Timonin. All rights reserved.
+//
+
+#include <iostream>
+
+class AbstractProductA {
+public:
+    virtual void paint() = 0;
+};
+
+class AbstractProductB {
+public:
+    virtual void paint() = 0;
+};
+
+class ProductA1 : public AbstractProductA {
+public:
+    void paint() {
+        std::cout << "ProductA1" << std::endl;
+    }
+};
+
+class ProductA2 : public AbstractProductA {
+public:
+    void paint() {
+        std::cout << "ProductA2" << std::endl;
+    }
+};
+
+class ProductB1 : public AbstractProductB {
+public:
+    void paint() {
+        std::cout << "ProductB1" << std::endl;
+    }
+};
+
+class ProductB2 : public AbstractProductB {
+public:
+    void paint() {
+        std::cout << "ProductB2" << std::endl;
+    }
+};
+
+class AbstractFactory {
+public:
+    virtual AbstractProductA* CreateProductA() = 0;
+    virtual AbstractProductB* CreateProductB() = 0;
+};
+
+class ConcreteFactory1 : public AbstractFactory {
+public:
+    AbstractProductA* CreateProductA() {
+        return new ProductA1;
+    }
+    AbstractProductB* CreateProductB() {
+        return new ProductB1;
+    }
+};
+
+class ConcreteFactory2 : public AbstractFactory {
+public:
+    AbstractProductA* CreateProductA() {
+        return new ProductA2;
+    }
+    AbstractProductB* CreateProductB() {
+        return new ProductB2;
+    }
+};
+
+int main()
+{
+    AbstractFactory *AF;
+    AbstractProductB *APB;
+    AbstractProductA *APA;
+
+    AF = new ConcreteFactory2;
+    APA = AF->CreateProductA();
+    APB = AF->CreateProductB();
+    
+    APA->paint();
+    APB->paint();
+    return 0;
+}
