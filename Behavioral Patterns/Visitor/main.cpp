9eb26b4f3a6c668f37018f67ce3396a08a1c3ec8
2gury <54157924+2gury@users.noreply.github.com>
2019-08-26 00:41:03 +0300

    Add Visitor pattern

---
+++ Behavioral Patterns/Visitor/main.cpp
@@ -0,0 +1,68 @@
+//
+//  main.cpp
+//  Visitor
+//
+//  Created by Egor Timonin on 25/08/2019.
+//  Copyright © 2019 Egor Timonin. All rights reserved.
+//
+
+class ConcreteElementA;
+class ConcreteElementB;
+
+class Visitor {
+public:
+    virtual void Visit(ConcreteElementA *element) = 0;
+    virtual void Visit(ConcreteElementB *element) = 0;
+};
+
+class Element {
+public:
+    virtual ~Element() {};
+    virtual void Accept(Visitor *concrete_visitor) = 0;
+};
+
+class ConcreteElementA : public Element {
+public:
+    void Accept(Visitor *concrete_visitor) {
+        concrete_visitor->Visit(this);
+    }
+    void OperationA1() {};
+    void OperationA2() {};
+};
+
+class ConcreteElementB : public Element {
+public:
+    void Accept(Visitor *concrete_visitor) {
+        concrete_visitor->Visit(this);
+    }
+    void OperationB1() {};
+    void OperationB2() {};
+};
+
+
+class ConcreteVisitor1 : public Visitor {
+public:
+    void Visit(ConcreteElementA *element) {
+        element->OperationA1();
+    };
+    void Visit(ConcreteElementB *element) {
+        element->OperationB1();
+    };
+};
+
+class ConcreteVisitor2 : public Visitor {
+public:
+    void Visit(ConcreteElementA *element) {
+        element->OperationA2();
+    }
+    void Visit(ConcreteElementB *element) {
+        element->OperationB2();
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
