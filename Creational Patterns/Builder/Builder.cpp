e9848bcfbea6be2786c016c144021432c83b2ff5
2gury <54157924+2gury@users.noreply.github.com>
2019-08-27 19:24:19 +0300

    Update Builder.cpp

--- Creational Patterns/Builder/Builder.cpp
+++ Creational Patterns/Builder/Builder.cpp
@@ -5,115 +5,86 @@ class Part1 {};
 class Part2 {};
 class Part3 {};
 class Part4 {};
-class Part5 {};
-class Part6 {};
 
 class Product {
 public:
-    std::vector <Part1> p1;
-    std::vector <Part2> p2;
-    std::vector <Part3> p3;
-    std::vector <Part4> p4;
-    std::vector <Part5> p5;
-    std::vector <Part6> p6;
+    std::vector<Part1*> vp1;
+    std::vector<Part2*> vp2;
+    std::vector<Part3*> vp3;
+    std::vector<Part4*> vp4;
 };
 
 class Builder {
-protected:
-    Product *pr;
 public:
-    virtual void BuildProduct(){};
-    virtual void BuildPart1(){};
-    virtual void BuildPart2(){};
-    virtual void BuildPart3(){};
-    virtual void BuildPart4(){};
-    virtual void BuildPart5(){};
-    virtual void BuildPart6(){};
-    virtual Product* GetResult() {return 0;}
+    virtual ~Builder() {};
+    virtual void CreateProduct() {};
+    virtual void BuildPart1() {};
+    virtual void BuildPart2() {};
+    virtual void BuildPart3() {};
+    virtual void BuildPart4() {};
+    virtual Product* GetResult() {return 0;};
 };
 
 class ConcreteBuilder1 : public Builder {
 public:
-    void BuildProduct() {
+    void CreateProduct() {
         pr = new Product;
     }
     void BuildPart1() {
-        pr->p1.push_back(Part1());
-        std::cout << "Part1" << std::endl;
+        pr->vp1.push_back(new Part1);
     }
     void BuildPart2() {
-        pr->p2.push_back(Part2());
-        std::cout << "Part2" << std::endl;
-    }
-    void BuildPart3() {
-        pr->p3.push_back(Part3());
-        std::cout << "Part3" << std::endl;
-    }
-    void BuildPart4() {
-        pr->p4.push_back(Part4());
-        std::cout << "Part4" << std::endl;
-    }
-    void BuildPart6() {
-        pr->p6.push_back(Part6());
-        std::cout << "Part6" << std::endl;
+        pr->vp2.push_back(new Part2);
     }
     Product* GetResult() {
         return pr;
     }
+private:
+    Product* pr;
 };
 
 class ConcreteBuilder2 : public Builder {
 public:
-    void BuildProduct() {
+    void CreateProduct() {
         pr = new Product;
     }
-    void BuildPart1() {
-        pr->p1.push_back(Part1());
-        std::cout << "Part1" << std::endl;
-    }
     void BuildPart3() {
-        pr->p3.push_back(Part3());
-        std::cout << "Part3" << std::endl;
+        pr->vp3.push_back(new Part3);
     }
     void BuildPart4() {
-        pr->p4.push_back(Part4());
-        std::cout << "Part4" << std::endl;
-    }
-    void BuildPart5() {
-        pr->p5.push_back(Part5());
-        std::cout << "Part5" << std::endl;
-    }
-    void BuildPart6() {
-        pr->p6.push_back(Part6());
-        std::cout << "Part6" << std::endl;
+        pr->vp4.push_back(new Part4);
     }
     Product* GetResult() {
         return pr;
     }
+private:
+    Product* pr;
 };
 
 class Director {
 public:
-    Product* Construct(Builder &builder) {
-        builder.BuildProduct();
-        builder.BuildPart1();
-        builder.BuildPart2();
-        builder.BuildPart3();
-        builder.BuildPart4();
-        builder.BuildPart5();
-        builder.BuildPart6();
-        return (builder.GetResult());
+    Director(Builder* builder_for_set): concrete_builder(builder_for_set) {};
+    void SetBuilder(Builder* builder_for_set) {
+        concrete_builder = builder_for_set;
     }
+    Product* Construct() {
+        concrete_builder->CreateProduct();
+        concrete_builder->BuildPart1();
+        concrete_builder->BuildPart2();
+        concrete_builder->BuildPart3();
+        concrete_builder->BuildPart4();
+        return concrete_builder->GetResult();
+    }
+private:
+    Builder* concrete_builder;
 };
 
 int main() {
-    Director director;
-    ConcreteBuilder1 builder1;
-    ConcreteBuilder2 builder2;
 
+    Director* director1 = new Director(new ConcreteBuilder1);
     
-    Product* product1 = director.Construct(builder1);
-    std::cout << std::endl;
-    Product* product2 = director.Construct(builder2);
+    Product* product1 = director1->Construct();
+    Director* director2 = new Director(new ConcreteBuilder2);
+    Product* product2 = director2->Construct();
     return 0;
 }
