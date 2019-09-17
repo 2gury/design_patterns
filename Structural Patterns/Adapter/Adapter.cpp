75fb736e309727526c5e45e2807429590a3604cf
2gury <54157924+2gury@users.noreply.github.com>
2019-09-17 23:12:48 +0300

    Update Adapter.cpp

--- Structural Patterns/Adapter/Adapter.cpp
+++ Structural Patterns/Adapter/Adapter.cpp
@@ -1,4 +1,10 @@
+//Example of `adapter' design pattern in C++
+
 #include <iostream>
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//The class adapter implementation////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 class AbstractTarget {
 public:
@@ -33,41 +39,45 @@ public:
     }
 };
 
-//class AbstractTarget {
-//public:
-//    virtual ~AbstractTarget() {};
-//    virtual void Request() = 0;
-//};
-//
-//class ConcreteTarget : public AbstractTarget {
-//public:
-//    void Request() {
-//        //...реализация
-//    }
-//};
-//
-//class AbstractAdaptee {
-//public:
-//    virtual ~AbstractAdaptee() {};
-//    virtual void SpecificRequest() = 0;
-//};
-//
-//class ConcreteAdaptee : public AbstractAdaptee {
-//public:
-//    void SpecificRequest() {
-//        //...адаптируемая реализация
-//    }
-//};
-//
-//class Adapter : public AbstractTarget {
-//public:
-//    Adapter(AbstractAdaptee* other_adaptee): adaptee(other_adaptee) {};
-//    void Request() {
-//        this->adaptee->SpecificRequest();
-//    }
-//protected:
-//    AbstractAdaptee* adaptee;
-//};
+///////////////////////////////////////////////////////////////////////////////////////////////
+//The object adapter implementation////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+class AbstractTarget {
+public:
+   virtual ~AbstractTarget() {};
+   virtual void Request() = 0;
+};
+
+class ConcreteTarget : public AbstractTarget {
+public:
+   void Request() {
+       //...реализация
+   }
+};
+
+class AbstractAdaptee {
+public:
+   virtual ~AbstractAdaptee() {};
+   virtual void SpecificRequest() = 0;
+};
+
+class ConcreteAdaptee : public AbstractAdaptee {
+public:
+   void SpecificRequest() {
+       //...адаптируемая реализация
+   }
+};
+
+class Adapter : public AbstractTarget {
+public:
+   Adapter(AbstractAdaptee* other_adaptee): adaptee(other_adaptee) {};
+   void Request() {
+       this->adaptee->SpecificRequest();
+   }
+protected:
+   AbstractAdaptee* adaptee;
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
