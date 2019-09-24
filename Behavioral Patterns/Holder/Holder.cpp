a8e76c3a946df388791fdf1cfd3da9bec9d589ca
2gury <54157924+2gury@users.noreply.github.com>
2019-09-24 13:43:42 +0300

    Update and rename main.cpp to Holder.cpp

--- Behavioral Patterns/Holder/Holder.cpp
+++ Behavioral Patterns/Holder/Holder.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Holder
-//
-//  Created by Egor Timonin on 24/09/2019.
-//  Copyright © 2019 Egor Timonin. All rights reserved.
-//
-
 #include <iostream>
 
 template <typename T>
